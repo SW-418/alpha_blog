af3fae5df91e174813ec8882925bd361ec6700c9
Sam Wells <samwells95@gmail.com>
2022-12-01 10:11:09 -0800

    Add ability to save articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,19 @@
 class ArticlesController < ApplicationController
 
-    def show()
+    def show
         @article = Article.find(params[:id])
     end
 
-    def index()
+    def index
         @articles = Article.all
     end
+ 
+    def new
+    end
 
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save()
+        redirect_to @article
+    end
 end
